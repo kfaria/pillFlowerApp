be0cb76b6806fcfb08e220976bf510b36ce21e37
Ken Faria <kennethfaria@gmail.com>
2017-04-26 15:21:25 -0400

    setup project files. All dev goes into app.js within src

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        {/*<Header headerText='Test' />*/}
+        <Text>An App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+//  import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//  make a component
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+};
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+
+//  make component available to other parts of the app
+export { Header };
+
