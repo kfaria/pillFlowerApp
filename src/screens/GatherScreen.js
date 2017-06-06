efe8eb4d6fea1d8330edb52250b63b9333407d50
Connor Dear <connordear@gmail.com>
2017-06-06 01:39:53 -0400

    added in animation for homescreen

--- src/screens/GatherScreen.js
+++ src/screens/GatherScreen.js
@@ -8,25 +8,8 @@ const { height, width } = Dimensions.get('window');
 
 const styles = {
   viewStyle: {
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     flex: 1,
-    borderWidth: 10,
-    borderColor: 'white',
-    justifyContent: 'flex-end',
-  },
-  scrollViewContainerStyle: {
-    borderWidth: 10,
-    borderColor: 'red',
-    flex: 1,
-  },
-  scrollViewStyle: {
-    borderWidth: 10,
-    borderColor: 'blue',
-    flex: 1,
-  },
-  imageStyle: {
-    height: 100,
-    width: 100,
   },
 };
 
@@ -43,75 +26,7 @@ class GatherScreen extends Component {
     return (
       <View style={styles.viewStyle}>
         <StatusBar hidden />
-        <View style={{ height: 100 }}>
-          <ScrollView
-            horizontal
-            contentContainerStyle={styles.scrollViewContainerStyle}
-            style={styles.scrollViewStyle}
-          >
-            <View style={{ position: 'absolute' }}>
-              <PlayFlower
-                imageSource={pic2}
-                width={200}
-                height={200}
-                maxSize={450}
-                minSize={50}
-              />
-            </View>
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-            <Image
-              source={require('../images/pill-flowers/01.png')}
-              style={styles.imageStyle}
-            />
-          </ScrollView>
-        </View>
+        <Image style={{ height, width }} source={require('../images/gatherPlaceholder.png')}/>
       </View>
     );
   }
